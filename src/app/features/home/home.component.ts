1a4819d51d495372d0f2707b71c945ff7d41eeb0
JonathanYaguana <jonathansacrox93@gmail.com>
2025-12-18 11:51:48 -0500

    style: cambio de nombre del selector de cada feature

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 import HeaderComponent from '../../layout/header/header-component';
+import { FooterComponent } from "../../layout/footer/footer-component";
+import AboutComponent from '../about/about.component';
+import SkillsComponent from '../skills/skills-component';
+import ProjectsComponent from '../projects/projects.component';
+import ContactComponent from '../contact/contact.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent
+  ],
   templateUrl: './home.component.html',
 })
 export default class HomeComponent {
