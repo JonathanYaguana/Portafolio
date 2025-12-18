49bc6fd34d30f5e03d2b59670115312e6f5e3495
JonathanYaguana <jonathansacrox93@gmail.com>
2025-12-18 15:46:04 -0500

    style: diseño del footer

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import HeaderComponent from '../../layout/header/header-component';
-import { FooterComponent } from "../../layout/footer/footer-component";
-import AboutComponent from '../about/about.component';
-import SkillsComponent from '../skills/skills-component';
-import ProjectsComponent from '../projects/projects.component';
-import ContactComponent from '../contact/contact.component';
+
+import HeaderComponent from '@shared/components/header/header-component';
+import FooterComponent from '@shared/components/footer/footer-component';
+
+import AboutComponent from '@features/about/about.component';
+import SkillsComponent from '@features/skills/skills-component';
+import ProjectsComponent from '@features/projects/projects.component';
+import ContactComponent from '@features/contact/contact.component';
 
 @Component({
   selector: 'app-home',
