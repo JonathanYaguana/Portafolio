f95cebf627209a21319fb833d617387812d21958
JonathanYaguana <jonathansacrox93@gmail.com>
2026-01-16 13:45:10 -0500

    add(about): se añadio la descripción personal

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'about-component',
-  imports: [],
   templateUrl: './about.component.html',
 })
 export default class AboutComponent {
